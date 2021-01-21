45be5f323c8583f1863c47314917a1ba3eeff97b
MEGU229 <74957333+MEGU229@users.noreply.github.com>
2021-01-21 16:19:29 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  ActionController::Base
+  # ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
