fa24ed0d5bebbd3d6aa464d1ad9d3d43069bcada
MEGU229 <74957333+MEGU229@users.noreply.github.com>
2021-01-22 20:04:47 +0900

    条件分岐

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
   def show
+    if user_signed_in?
     @user = User.find(params[:id])
     @manuals = @user.manuals
     favorites = Favorite.where(user_id: current_user.id).pluck(:manual_id)
     @favorite_manuals = Manual.where(id: favorites).includes(:user)
+    else
+    redirect_to root_path
+    end
   end
 end
